612e6dccd89727559d2001985bb5a403b390e1c7
V Suhas <suhas3145@gmail.com>
2023-11-03 12:56:40 +0530

    Pages and components file added

--- artisian/src/App.js
+++ artisian/src/App.js
@@ -1,17 +1,21 @@
-
-import { BrowserRouter,Route,Routes} from "react-router-dom";
-
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Landing from './pages/Landing';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 
 function App() {
   return (
-
-    <div className="App">
-
-    <h1>Hello</h1>
-
-    </div>
-
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
 
   );
 }

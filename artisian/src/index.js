95eeea7fada1f5a9e2ee778c5afbaf1d430fb4e7
Avinash7390 <avinashk82495@gmail.com>
2023-11-08 15:46:46 +0530

    Dark/Light Mode added

--- artisian/src/index.js
+++ artisian/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ColorModeScript, ChakraProvider, theme } from "@chakra-ui/react";
+
+import ColorModeSwitcher from "./ColorModeSwitcher";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <ColorModeScript />
+    <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
+      <App />
+    </ChakraProvider>
   </Router>
 );

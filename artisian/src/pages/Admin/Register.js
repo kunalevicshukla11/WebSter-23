5c9bb18a653e47d7aaa11bcc83fab80b62ab1a36
IAMAmanRaj <imamanraj87@gmail.com>
2023-11-15 01:00:29 +0530

    login/sign up for user/admin done

--- artisian/src/pages/Admin/Register.js
+++ artisian/src/pages/Admin/Register.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../../CSS/RegisterStudent.css";
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import axios from "axios";
-import Footer from "../../components/Footer";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -25,7 +24,7 @@ const Register = () => {
 
       if (res.data.success) {
         toast.success(res.data.message);
-        navigate("/dashboard");
+        navigate("/login-admin");
       }
     } catch (error) {
       const msg = error.response.data.message;
@@ -35,68 +34,93 @@ const Register = () => {
   };
 
   return (
-    <div className="containerRUser">
-      <div className="cardRUser">
-        <div className="card_titleRUser">
-          <h1>Create Account</h1>
-          <span>
-            Already have an account?{" "}
-            <NavLink to="/login-admin">Sign In</NavLink>
-          </span>
-        </div>
-        <div className="form formRUser">
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <img
+          className="h-screen w-screen object-cover "
+          src="/images/dark2.png"
+          alt="loginbg"
+        />
+
+        <div className="absolute w-[300px] h-[450px]  sm:w-[400px] sm:h-[470px] p-8 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-teal-950  text-white opacity-60">
+          <div className="text-center">
+            <h1 className="text-3xl">Create Account</h1>
+            <span className="  text-[17px]">
+              Already have an account?{" "}
+              <NavLink className="font-bold" to="/login-admin">
+                Sign In
+              </NavLink>
+            </span>
+          </div>
           <form action="/register" method="post" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="UserName"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-            <input
-              type="number"
-              name="studentid"
-              id="studentid"
-              placeholder="Hostel ID"
-              value={HostelID}
-              onChange={(e) => setHostelID(e.target.value)}
-              required
-            />
-            <input
-              type="text"
-              name="Hostel"
-              id="Hostel"
-              placeholder="Hostel Name"
-              value={HostelName}
-              onChange={(e) => setHostelName(e.target.value)}
-              required
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <button>Sign Up</button>
+            <div className="flex flex-col py-4 gap-2 px-2">
+              <input
+                className="p-2 text-lg font-mono font-bold bg-pink-950"
+                type="text"
+                name="username"
+                id="username"
+                placeholder="UserName"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <input
+                className="p-2 text-lg font-mono font-bold bg-pink-950"
+                type="number"
+                name="studentid"
+                id="studentid"
+                placeholder="Hostel ID"
+                value={HostelID}
+                onChange={(e) => setHostelID(e.target.value)}
+                required
+              />
+
+              <input
+                className="p-2 text-lg font-mono font-bold bg-pink-950"
+                type="text"
+                name="Hostel"
+                id="Hostel"
+                placeholder="Hostel Name"
+                value={HostelName}
+                onChange={(e) => setHostelName(e.target.value)}
+                required
+              />
+
+              <input
+                className="p-1.5 text-lg font-mono font-bold bg-pink-950"
+                type="email"
+                name="email"
+                placeholder="Email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                className="w-full p-2 text-lg font-mono font-bold h-[35px] bg-pink-950 "
+                type="password"
+                name="password"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <p className="flex ml-2 items-center">
+              <input className="mr-2" type="checkbox" />
+              Remember me
+            </p>
+            <button
+              className="border-2 w-full my-5 py-4 hover:bg-zinc-950  font-bold"
+              type="submit"
+            >
+              Sign In
+            </button>
           </form>
         </div>
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 

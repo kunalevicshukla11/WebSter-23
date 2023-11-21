ab2d8689456c311bfc72bd208ca49efb13b0b897
Avinash7390 <avinashk82495@gmail.com>
2023-11-21 13:02:26 +0530

    Minor Changes

--- artisian/src/utils/Comp.js
+++ artisian/src/utils/Comp.js
@@ -2,10 +2,22 @@ import React from "react";
 import WrapperComp from "../StyledComponents/WrapperComp.js";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/userContext.js";
+import { useToast } from "@chakra-ui/react";
 
 const Comp = (props) => {
   const data = props.compData;
   const [auth, setAuth] = useAuth();
+  const toast = useToast();
+
+  const handleClick = () => {
+    toast({
+      title: `First see the complaint`,
+      description: "Invalid request",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
   return (
     <WrapperComp>
       <header>
@@ -26,7 +38,11 @@ const Comp = (props) => {
             </Link>
             {auth.user.role === 1 ? (
               <>
-                <button type="button" className="btn delete-btn">
+                <button
+                  type="button"
+                  className="btn delete-btn"
+                  onClick={handleClick}
+                >
                   Resolve
                 </button>
               </>

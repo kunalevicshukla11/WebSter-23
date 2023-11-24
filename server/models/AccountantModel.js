adbcc3a7c374b77c0112fbf1b285496eec02f3b4
Avinash7390 <avinashk82495@gmail.com>
2023-11-25 02:23:03 +0530

    Email Validation

--- server/models/AccountantModel.js
+++ server/models/AccountantModel.js
@@ -1,36 +1,42 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const AccountantSchema = new mongoose.Schema(
-    {
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-
-      HostelID: {
-        type: Number,
-        required: true,
-      },
-      HostelName: {
-        type: String,
-        required: true,
-      },
-      role: {
-        type: Number,
-        default: 3,
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide a valid email",
       },
     },
-    { timestamps: true }
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    HostelID: {
+      type: Number,
+      required: true,
+    },
+    HostelName: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 3,
+    },
+  },
+  { timestamps: true }
 );
 
 export const accountantModel = mongoose.model("Accountant", AccountantSchema);
--- server/models/AdminModel.js
+++ server/models/AdminModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const AdminSchema = new mongoose.Schema(
   {
@@ -11,10 +12,15 @@ const AdminSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide a valid email",
+      },
     },
     password: {
       type: String,
       required: true,
+      minLength: 6,
     },
     HostelID: {
       type: Number,
